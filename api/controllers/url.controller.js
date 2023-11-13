861fb6ff89c0c21dff288e5ba293340d2dfa7f48
gyanchith28 <gyanchith@gmail.com>
2023-11-13 22:49:43 +0530

    Store password in encypted manner

--- api/controllers/url.controller.js
+++ api/controllers/url.controller.js
@@ -1,8 +1,10 @@
 import Url from "../modals/urls.lock.js";
 import { nanoid } from "nanoid";
+import bcryptjs from 'bcryptjs';
 
 async function handleGenerateShortUrl(req, res) {
   const { redirectUrl, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
   if (!redirectUrl || !password)
     return res.status(400).json({ error: "url and password required" });
   const shortUrl = nanoid(8);
@@ -11,7 +13,7 @@ async function handleGenerateShortUrl(req, res) {
     const url = await Url.create({
       redirectUrl,
       shortUrl,
-      password,
+      password: hashedPassword,
     });
 
     res.status(201).json({ shortUrl: url.shortUrl });
@@ -31,8 +33,8 @@ async function handleUnlockShortUrl(req, res) {
     if (!url) {
       return res.status(404).json({ error: "Link not found" });
     }
-
-    if (password === url.password) {
+    const validPassword = bcryptjs.compareSync(password, url.password)
+    if (validPassword) {
       res.redirect(url.redirectUrl);
     } else {
       return res.status(401).json({ error: "Invalid password" });
