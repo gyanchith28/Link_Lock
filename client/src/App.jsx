061ac4449c38332997b41a36834a7e1ef75e7e81
gyanchith28 <gyanchith@gmail.com>
2023-11-14 17:41:27 +0530

    Add Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LockUrl from "./pages/LockUrl";
 import UnlockUrl from "./pages/UnlockUrl";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<LockUrl />} />
         <Route path="/unlock" element={<UnlockUrl />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='bg-[#ce5a67]'>
+        <h1 className='text-white shadow-xl font-yatraOne text-4xl sm:text-6xl flex justify-center p-8'>
+            Link Lock
+        </h1>
+    </header>
+  )
+}
