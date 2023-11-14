0ca6cf75fe4559cd092020440fb824d84909c223
gyanchith28 <gyanchith@gmail.com>
2023-11-14 15:09:23 +0530

    Add lock n unlock pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LockUrl from "./pages/LockUrl";
+import UnlockUrl from "./pages/UnlockUrl";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LockUrl />} />
+        <Route path="/unlock" element={<UnlockUrl />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
