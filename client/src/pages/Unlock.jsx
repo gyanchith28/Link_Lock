ab0b7807bd96a1709a28519ae08c4c2c738669b9
gyanchith28 <gyanchith@gmail.com>
2023-11-25 12:25:12 +0530

    Redirect after unlocking URL

--- client/src/pages/Unlock.jsx
+++ client/src/pages/Unlock.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import CardComponent from "../components/CardComponent";
+import axios from "axios";
 
 export default function Unlock() {
-  return <div>Unlock</div>;
+  const handleButtonClick = async (formData) => {
+    try {
+      const res = await axios.post("/api/unlock", formData);
+      console.log(res.data);
+      let redirectUrl = res.data.url;
+      window.location = redirectUrl;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <div>
+      <h1>Unlock Url</h1>
+      <CardComponent
+        heading="Card Heading for Route 1"
+        description="Description for Route 1"
+        urlId="shortUrl"
+        buttonText="Unlock Url"
+        onButtonClick={handleButtonClick}
+      />
+    </div>
+  );
 }
